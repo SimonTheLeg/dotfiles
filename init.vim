08b344322859552ae202aca1d834c7579019ca7d
Simon Bein <simontheleg@gmail.com>
2019-11-30 17:06:10 +0100

    add NeoVim Config

---
+++ init.vim
@@ -0,0 +1,5 @@
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+let mapleader =" "
+source ~/.vimrc
+
--- linkall.sh
+++ linkall.sh
@@ -4,3 +4,4 @@ ln -s -f ${DOTFILESPATH}/.vimrc ~/.vimrc
 ln -s -f ${DOTFILESPATH}/.gitconfig ~/.gitconfig
 ln -s -f ${DOTFILESPATH}/.zshrc ~/.zshrc
 ln -s -f ${DOTFILESPATH}/starship.toml ~/.config/starship.toml
+ln -s -f ${DOTFILESPATH}/init.vim ~/.config/nvim/init.vim
