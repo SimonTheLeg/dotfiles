fb68909f0cd6a197db77affcd5fdc8989c495562
Simon Bein <simontheleg@gmail.com>
2025-02-05 20:56:50 +0100

    add mkDocs mike

--- home.nix
+++ home.nix
@@ -190,6 +190,7 @@ in {
     oauth2c
     rclone
     jfrog-cli
+    python312Packages.mike # muli mkDocs management
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
