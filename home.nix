9600df261b059616a6933573cebaca13f1a9f715
Simon Bein <simontheleg@gmail.com>
2025-02-05 20:54:31 +0100

    add gnutar

--- home.nix
+++ home.nix
@@ -179,6 +179,7 @@ in {
     opentofu
     gawk
     git-crypt
+    gnutar
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
