b84f7b1cb9e9758ad4a2f2c9a1e1313e603e7822
Simon Bein <simontheleg@gmail.com>
2025-02-05 20:56:07 +0100

    add rclone

--- home.nix
+++ home.nix
@@ -188,6 +188,7 @@ in {
     ory
     jwt-cli
     oauth2c
+    rclone
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
