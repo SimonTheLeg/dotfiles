19028c588bd0d30c13528a7ea96c2c8801690fae
Simon Bein <simontheleg@gmail.com>
2025-02-05 20:56:02 +0100

    add oauth2c

--- home.nix
+++ home.nix
@@ -187,6 +187,7 @@ in {
     kratos
     ory
     jwt-cli
+    oauth2c
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
