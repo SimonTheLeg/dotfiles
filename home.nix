29056f89eef2d0a2df3738dbc4bc22d79b338285
Simon Bein <simontheleg@gmail.com>
2025-02-05 20:52:38 +0100

    add openssl + cfssl

--- home.nix
+++ home.nix
@@ -171,6 +171,8 @@ in {
     pstree
     yarn
     cmake
+    openssl
+    cfssl
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
