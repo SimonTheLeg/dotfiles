fc3fb46e75874cbf64295958a5fad18ec08ce570
Simon Bein <simontheleg@gmail.com>
2025-02-05 20:55:55 +0100

    add jwt-cli

--- home.nix
+++ home.nix
@@ -186,6 +186,7 @@ in {
     nix-prefetch
     kratos
     ory
+    jwt-cli
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
