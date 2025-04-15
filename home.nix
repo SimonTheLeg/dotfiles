52001aec269ff59246509cc483d8ec5523071b43
Simon Bein <simontheleg@gmail.com>
2025-04-15 14:25:43 +0200

    add Taskfile binary

--- home.nix
+++ home.nix
@@ -194,6 +194,7 @@ in {
     riffdiff
     podman
     fd
+    go-task
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
