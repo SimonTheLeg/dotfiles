71116c0808137772b7e549f22dd0e6a7ac0143b4
Simon Bein <simontheleg@gmail.com>
2025-04-17 10:42:21 +0200

    include hidden files when searching with telescope find_files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,14 @@
 local M = {}
 
 M.Setup = function()
+  require('telescope').setup {
+    pickers = {
+      find_files = {
+        hidden = true
+      }
+    }
+  }
+
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
   vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
