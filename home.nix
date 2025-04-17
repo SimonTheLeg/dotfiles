db4d0b2ff3f73f314b111f5423e01734e046aa3f
Simon Bein <simontheleg@gmail.com>
2025-04-17 10:40:43 +0200

    add ansifilter

--- home.nix
+++ home.nix
@@ -195,6 +195,7 @@ in {
     podman
     fd
     go-task
+    ansifilter
   ];
   # for future Simon: if I ever need more than one channel as source, here's how to do it https://discourse.nixos.org/t/nix-env-i-runs-out-of-memory-with-unstable-overlay/1517/3
 
