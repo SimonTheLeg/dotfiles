f2a9d18c358f7a846ee445feae7ec30624a06b3d
Simon Bein <simontheleg@gmail.com>
2026-02-28 14:49:17 +0100

    add telescope keymaps for search keymaps and vim commands

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -41,6 +41,8 @@ M.Setup = function()
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
   vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+  vim.keymap.set('n', '<C-P>', builtin.commands, { desc = 'Telescope search vim commands' })
+  vim.keymap.set('n', '<leader>?', builtin.keymaps, { desc = 'Telescope list keymaps' })
 end
 
 return M
