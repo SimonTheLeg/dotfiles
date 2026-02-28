c709d31124528823d05ee32cca32cde30ba82590
Simon Bein <simontheleg@gmail.com>
2026-02-28 14:42:54 +0100

    add descriptions to git commands, so they are searchable

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -12,20 +12,20 @@ M.Setup = function()
         vim.keymap.set(mode, l, r, opts)
       end
       -- hunk staging
-      map('n', '<leader>ga', gs.stage_hunk)
-      map('n', '<leader>gr', gs.reset_hunk)
+      map('n', '<leader>ga', gs.stage_hunk, { desc = "git stage hunk" })
+      map('n', '<leader>gr', gs.reset_hunk, { desc = "git reset hunk" })
       map('v', '<leader>ga', function()
         gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-      end)
+      end, { desc = "git stage hunk" })
       map('v', '<leader>gr', function()
         gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-      end)
+      end, { desc = "git reset hunk" })
       -- stage the whole file/buffer
-      map('n', '<leader>gS', gs.stage_buffer)
-      map('n', '<leader>gR', gs.reset_buffer)
+      map('n', '<leader>gS', gs.stage_buffer, { desc = "git stage file" })
+      map('n', '<leader>gR', gs.reset_buffer, { desc = "git reset file" })
       -- preview hunk
-      map('n', '<leader>gp', gs.preview_hunk)
-      map('n', '<leader>gi', gs.preview_hunk_inline)
+      map('n', '<leader>gp', gs.preview_hunk, { desc = "git preview hunk" })
+      map('n', '<leader>gi', gs.preview_hunk_inline, { desc = "git preview hunk inline" })
       -- hunk navigation
       -- Navigation
       map('n', ']c', function()
@@ -34,14 +34,14 @@ M.Setup = function()
         else
           gs.nav_hunk('next')
         end
-      end)
+      end, { desc = "git next hunk" })
       map('n', '[c', function()
         if vim.wo.diff then
           vim.cmd.normal({ '[c', bang = true })
         else
           gs.nav_hunk('prev')
         end
-      end)
+      end, { desc = "git previous hunk" })
     end
   }
 
